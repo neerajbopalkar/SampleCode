d7c887543cfcbe81546effe7231c0bbe1e0ae6e9
neerajbopalkar <neeraj.bopalkar@gmail.com>
2020-08-02 23:24:05 +0530

    Added weather service

---
+++ 01.DotNetCoreWebAPIs/WeatherService/WebApplication1/Services/WeatherService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Services
+{
+    public class WeatherService : IWeatherService
+    {
+        private readonly ILogger<WeatherService> _logger;
+        public WeatherService(ILogger<WeatherService> logger)
+        {
+            _logger = logger;
+        }
+
+        public IEnumerable<WeatherForecast> GetWeatherList()
+        {
+            _logger.LogInformation("In WeatherService:: GetWeatherList()");
+            var Summaries = new[]
+      {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
