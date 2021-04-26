17f0af4f9406aca5e4db3c88bfb5453d89851dff
Neeraj <neeraj.bopalkar@gmail.com>
2021-04-26 22:35:49 +0530

    performance profiling using Glimpse Performance improvement in DAL, Application layer & Client - all 3 layers using output cache, release bundle, session disabling, JS and CSS bundling

--- 03.AspDotNetMvc5/Vidly/Vidly/Controllers/HomeController.cs
+++ 03.AspDotNetMvc5/Vidly/Vidly/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace Vidly.Controllers
 {
     public class HomeController : Controller
     {
+        //do not apply this attribute AFTER profiling page, not in beginning
+        // it will show stale data. don't assume page will be slow
+        [OutputCache(Duration = 50, Location = OutputCacheLocation.Server, VaryByParam = "")]
         public ActionResult Index()
         {
             return View();
