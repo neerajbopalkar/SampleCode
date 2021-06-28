b8465a9be3de5fc1ab41d5c8716662d62945e9ef
Neeraj <neeraj.bopalkar@gmail.com>
2021-06-28 13:26:51 +0530

    React - added new react app for order counters 1. create-react-app 2. import bootstrap

---
+++ 06.React/counter-app/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+//getting error below on only importing bootstrap. Hence need to install jquery and popper.js
+//: https://stackoverflow.com/questions/49589282/bootstrap-in-react-cant-resolve-jquery-module
+import $ from 'jquery';
+import Popper from 'popper.js'; 
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
